6b202f2eb2ffe689aac73d2f83491d6794920cb8
Swetank Mohanty (shortthirdman) <swetank.mohanty@outlook.com>
2021-04-24 22:07:16 +0530

    Turn on/off Windows Firewall

--- Miscellaneous/ControlNetworks.ps1
+++ Miscellaneous/ControlNetworks.ps1
@@ -3,3 +3,11 @@ Get-NetIPConfiguration -All
 Get-NetIPConfiguration | Get-NetIPAddress
 (Get-WmiObject -Class Win32_NetworkAdapterConfiguration).Where{ $_.IPAddress } | Format-List
 Get-NetAdapter
+
+# Turn off Windows Firewall
+netsh advfirewall set allprofiles state off
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+
+# Turn on Windows Firewall
+netsh advfirewall set allprofiles state on
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
